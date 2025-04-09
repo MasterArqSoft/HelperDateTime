b763ba6c228d57defd1954c09cbe41fd0320d06d
Erwing Candelario <ingeniero_79@hotmail.com>
2025-04-08 23:33:16 -0500

    Creación del proyecto de consola para probar las funcionalidades DateTime, se creo la funcionalidad obtener la fecha actual indicando la zona horaria y validar si un año es bisiesto

---
+++ ConsoleAppDateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelperDateTime;
+
+Console.WriteLine("======== Prueba funcionalidades de fechas ===========");
+Console.WriteLine($" DateTime.UtcNow: {DateTime.UtcNow} - DateTime.Now {DateTime.Now} - DateTime.Today:  {DateTime.Today}");
+try
+{
+    Console.WriteLine(value: $"El Resultado Current Date: {DateQuery.CurrentDateConvertUtcLocal("America/Bogota")}");
+    Console.WriteLine(value: $"El Resultado si el año es bisiesto: {DateQuery.IsLeapYear(0)}");
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(value: $"Error {ex.Message}");
+}
